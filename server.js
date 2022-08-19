52198b9c80f3c6cd47f56e6216ec2580d0eb03b1
Stephen Blevins <scblevins27@gmail.com>
2022-08-18 23:04:27 -0400

    initial commit to heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const cors = require('cors')
+const path = require("path")
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -24,5 +25,18 @@ app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/logs", require("./routes/logRoutes"));
 
 app.use(errorHandler);
+
+// Serve client
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+  
+    app.get("*", (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, "../", "frontend", "build", "index.html")
+      )
+    );
+  } else {
+    app.get("/", (req, res) => res.send("Please set to production"));
+  }
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
